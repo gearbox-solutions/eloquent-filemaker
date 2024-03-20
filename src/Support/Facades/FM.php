d79fb4dcbe284908a7f13593ffe90ac9e0400d95
David Nahodyl <david@gearboxgo.com>
2024-03-20 17:27:16 -0400

    ping formatting

--- src/Support/Facades/FM.php
+++ src/Support/Facades/FM.php
@@ -14,13 +14,10 @@ use Illuminate\Support\Facades\Facade;
  * @method static FMBaseBuilder table($layoutName)
  * @method static FMBaseBuilder delete($recordId)
  * @method static FMBaseBuilder deleteByRecordId($recordId)
-
-
  * @method static array setGlobalFields(array $globalFields)
  * @method static FileMakerConnection connection(string $name = null)
  * @method static FileMakerConnection setRetries(int $retries)
  * @method static FileMakerConnection getLayoutMetadata($layoutName = null)
-
  *
  * @see \Illuminate\Database\DatabaseManager
  * @see FileMakerConnection
