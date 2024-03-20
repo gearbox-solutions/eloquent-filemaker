5325e68f0536d4c02f9cbe844977cd3eea1bfe8f
David Nahodyl <david@gearboxgo.com>
2024-03-20 17:25:19 -0400

    pint formatting

--- src/Database/Eloquent/FMEloquentBuilder.php
+++ src/Database/Eloquent/FMEloquentBuilder.php
@@ -74,6 +74,7 @@ class FMEloquentBuilder extends Builder
                 throw $e;
             }
         }
+
         // It didn't error, so we have something
         return true;
     }
@@ -263,8 +264,8 @@ class FMEloquentBuilder extends Builder
     /**
      * Compares a model's modified portal data and original portal data and returns portal data with only modified fields and recordIds
      *
-     * @param $array1 array The modified portal data
-     * @param $array2 array The model's original portal data
+     * @param  $array1  array The modified portal data
+     * @param  $array2  array The model's original portal data
      */
     protected function getOnlyModifiedPortalFields($array1, $array2): array
     {
