af0ec9662203087c04006e146295c59774076c25
David Nahodyl <david@gearboxgo.com>
2024-05-24 15:19:49 -0400

    removed unused import

--- src/Database/Eloquent/FMEloquentBuilder.php
+++ src/Database/Eloquent/FMEloquentBuilder.php
@@ -7,7 +7,6 @@ use GearboxSolutions\EloquentFileMaker\Exceptions\FileMakerDataApiException;
 use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Scope;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Arr;
 
